2cbcbbc87d990500a105b8efebd650e5e7af9d2e
bjoernh2000 <bjoernh@bu.edu>
2020-10-17 22:44:57 +0200

    added map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Map } from "./components/Map";
 
 export class App extends Component {
   render() {
     return (
       <div>
         <h1>What to Do</h1>
+        <Map></Map>
       </div>
     );
   }
